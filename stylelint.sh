c85355ee80384413424cd23f845c8290336b0291
杨魁 <yangkui.thoamsy@bytedance.com>
2020-03-27 19:44:55 +0800

    stylelint 脚本代码

---
+++ stylelint.sh
@@ -0,0 +1,32 @@
+
+if [ -f "yarn.lock" ]; then
+  yarn add -D stylelint stylelint-config-recommended
+else
+  npm add -D stylelint stylelint-config-recommended
+fi
+
+
+if [ ! -f '.stylelintrc.json' ]; then
+  echo '{
+    "extends": "stylelint-config-recommended",
+    "rules": {
+      "no-descending-specificity": null,
+      "declaration-colon-newline-after": null,
+      "at-rule-no-unknown": null
+    }
+  }' > .stylelintrc.json
+fi
+
+lintTest=$(echo '.foo {}' | npx stylelint)
+
+if [ ! -z lintTest ]; then
+  echo '测试 Lint 成功!'
+fi
+
+filterLint=$(code --list-extensions | grep 'stylelint.vscode-stylelint')
+
+if [ -z filterLint ]; then
+  echo '正在安装 vscode—stylelint 插件‘
+  code  --install-extension stylelint.vscode-stylelint
+fi
+
