7574fd0e48885e5002163cddecc618b9b29d7132
杨魁 <c6swift@gmail.com>
2020-04-02 11:59:28 +0800

    fix: 删除无用代码

--- lint-for-vue.js
+++ lint-for-vue.js
@@ -1,39 +1,38 @@
-const { promisify } = require('util');
-const path = require('path');
-const { readFileSync, writeFileSync } = require('fs');
-const { spawn } = require('child_process');
-const { plugins } = require('../development_workflow/.eslintrc');
+const { promisify } = require("util");
+const path = require("path");
+const { readFileSync, writeFileSync } = require("fs");
+const { spawn } = require("child_process");
 
 let packageTool;
 try {
-  readFileSync(path.resolve(process.cwd(), './yarn.lock'));
-  packageTool = 'yarn';
+  readFileSync(path.resolve(process.cwd(), "./yarn.lock"));
+  packageTool = "yarn";
 } catch {
-  packageTool = 'npm';
+  packageTool = "npm";
 }
 
 const spawnChild = async (command, options) => {
   console.log(`${packageTool} ${options}`);
-  console.log('请耐心等待，没有进度条🚗');
-  const result = spawn(packageTool, options.split(' '));
+  console.log("请耐心等待，没有进度条🚗");
+  const result = spawn(packageTool, options.split(" "));
   for await (const chunk of result.stdout) {
-    console.log(chunk.toString('utf8'));
+    console.log(chunk.toString("utf8"));
   }
   for await (const chunk of result.stderr) {
-    console.error(chunk.toString('utf8'));
+    console.error(chunk.toString("utf8"));
   }
 };
 
 function updatePackageJSON() {
-  const packagePath = path.resolve(process.cwd(), 'package.json');
+  const packagePath = path.resolve(process.cwd(), "package.json");
   try {
     const packageJSON = JSON.parse(
-      readFileSync(packagePath, { encoding: 'utf8' }),
+      readFileSync(packagePath, { encoding: "utf8" })
     );
 
-    const lintStagedKey = 'lint-staged';
-    const jsKEY = '**/*.{js,ts,vue}';
-    const configList = ['pretty-quick --staged', 'eslint'];
+    const lintStagedKey = "lint-staged";
+    const jsKEY = "**/*.{js,ts,vue}";
+    const configList = ["pretty-quick --staged", "eslint"];
     if (packageJSON[lintStagedKey]) {
       packageJSON[lintStagedKey][jsKEY] = configList;
     } else {
@@ -45,12 +44,12 @@ function updatePackageJSON() {
     if (packageJSON.husky) {
       Object.assign(packageJSON.husky.hooks, {
         ...packageJSON.husky.hooks,
-        'pre-commit': 'lint-staged',
+        "pre-commit": "lint-staged",
       });
     } else {
       packageJSON.husky = {
         hooks: {
-          'pre-commit': 'lint-staged',
+          "pre-commit": "lint-staged",
         },
       };
     }
@@ -59,42 +58,44 @@ function updatePackageJSON() {
       packageJSON.scripts.format = `prettier --write \"./src/**/*.{js,ts,vue}\"`;
     }
     writeFileSync(packagePath, JSON.stringify(packageJSON, null, 2));
-    console.log('写入 lint-staged 成功');
+    console.log("写入 lint-staged 成功");
     return true;
   } catch {
-    console.error('解析 package.json 失败，你的东西有点问题');
+    console.error("解析 package.json 失败，你的东西有点问题");
     return false;
   }
 }
 
 function writeESLint() {
   try {
-    const eslintPath = path.resolve(process.cwd(), '.eslintrc.js');
+    const eslintPath = path.resolve(process.cwd(), ".eslintrc.js");
     const eslintConfig = require(eslintPath);
-    eslintConfig.parser = 'vue-eslint-parser';
+    eslintConfig.parser = "vue-eslint-parser";
     eslintConfig.parserOptions = {
-      parser: 'babel-eslint',
-      sourceType: 'module',
+      parser: "babel-eslint",
+      sourceType: "module",
     };
 
     eslintConfig.extends = []
       .concat(eslintConfig.extends || [])
-      .filter(preset => !preset.endsWith('prettier') && !preset.endsWith('vue'))
-      .concat(['plugin:vue/base', 'plugin:prettier/recommended']);
+      .filter(
+        (preset) => !preset.endsWith("prettier") && !preset.endsWith("vue")
+      )
+      .concat(["plugin:vue/base", "plugin:prettier/recommended"]);
 
     eslintConfig.plugins = []
       .concat(eslintConfig.plugins || [])
       .filter(
-        plugin => !plugin.endsWith('html') && !plugin.endsWith('prettier'),
+        (plugin) => !plugin.endsWith("html") && !plugin.endsWith("prettier")
       )
-      .concat('vue');
+      .concat("vue");
 
     writeFileSync(
       eslintPath,
-      'module.exports = ' + JSON.stringify(eslintConfig, null, 2),
+      "module.exports = " + JSON.stringify(eslintConfig, null, 2)
     );
 
-    console.log('修改 eslint 配置成功 🎉');
+    console.log("修改 eslint 配置成功 🎉");
     return true;
   } catch (err) {
     console.error(err);
@@ -102,36 +103,41 @@ function writeESLint() {
   }
 }
 function writePrettier() {
-  const prettierPath = path.resolve(process.cwd(), '.prettierrc');
-  writeFileSync(
-    prettierPath,
-    JSON.stringify(
-      {
-        arrowParens: 'always',
-        semi: true,
-        singleQuote: true,
-        printWidth: 80,
-        useTabs: false,
-        tabWidth: 2,
-        trailingComma: 'es5',
-      },
-      null,
-      2,
-    ),
-  );
-  console.log('写入 prettier 配置成功🎉');
-  return true;
+  const prettierPath = path.resolve(process.cwd(), ".prettierrc");
+  try {
+    writeFileSync(
+      prettierPath,
+      JSON.stringify(
+        {
+          arrowParens: "always",
+          semi: true,
+          singleQuote: true,
+          printWidth: 80,
+          useTabs: false,
+          tabWidth: 2,
+          trailingComma: "es5",
+        },
+        null,
+        2
+      )
+    );
+    console.log("写入 prettier 配置成功🎉");
+    return true;
+  } catch (err) {
+    console.error("创建 prettier 配置失败");
+    return false;
+  }
 }
 
 (async function go() {
-  await spawnChild(packageTool, 'remove eslint-plugin-html');
+  await spawnChild(packageTool, "remove eslint-plugin-html");
   await spawnChild(
     packageTool,
-    'add -D eslint@^6 prettier eslint-config-prettier eslint-plugin-prettier vue-eslint-parser eslint-plugin-vue@^6 husky pretty-quick',
+    "add -D eslint@^6 prettier eslint-config-prettier eslint-plugin-prettier vue-eslint-parser eslint-plugin-vue@^6 husky pretty-quick"
   );
 
   writeESLint() &&
     writePrettier() &&
     updatePackageJSON() &&
-    console.log('为了最好的效果，请先 reload vscode');
+    console.log("为了最好的效果，请先 reload vscode");
 })();
